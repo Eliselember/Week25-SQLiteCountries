b24bb590aa8732cc0a53d1e83d929b33039bcac3
Opilane TTHK <opilane@tthk.ee>
2022-02-18 11:00:35 +0200

    week25

---
+++ SQliteCountries/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQliteCountries
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source = SQliteCountries.db");
+        }
+
+        public void OpenConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Open();
+            }
+        }
+        
+    }
+}
---
+++ SQliteCountries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQliteCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void ReadCountries()
+        {
+            Database databaseObj = new Database();
+            string query = "SELECT Country.countryname AS Country, Capital.capitalname AS Capital Join Country on Capital.countryId = Country.rowid";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObj.myConnection);
+            databaseObj.OpenConnection();
+
+            SQLiteDataReader data = myCommand.ExecuteReader();
+
+            if (data.HasRows)
+            {
+                while (data.Read())
+                {
+                    Console.WriteLine($"Country; {data["Country"]}. Capital: {data["Capital"]}");
+
+                }
+            }
+            databaseObj.CloseConnection();
+        }
+    }
+}
